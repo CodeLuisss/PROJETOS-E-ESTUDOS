aa2bbe7d552226208497d3d0f502e14397dfcac5
CodeLuisss <luishenrique.mendes2004@gmail.com>
2025-11-24 15:07:54 -0300

    Melhorias no código da calculadora

--- Exercises/8 - calculadora.py
+++ Exercises/8 - calculadora.py
@@ -1,37 +1,31 @@
 # CALCULADORA SIMPLES
 
-# AQUI EU VOU PEDIR PARA O USUÁRIO SELECIONAR QUAL OPERAÇÃO MATEMÁTICA ELE QUER FAZER
-
-operacao = int(input('Selecione qual operação quer fazer: '))
 print('1. Adição')
 print('2. Subtração')
 print('3. Divisão')
 print('4. Multiplicação')
 
+operacao = int(input('Selecione qual operação quer fazer: '))
 
-# AQUI O USUÁRIO VAI DAR ENTRADA EM DOIS NÚMEROS INTEIROS
 
 x = int(input('Digite o primeiro número: '))
 y = int(input('Digite o segundo número: '))
 
 
-# AQUI SERÁ ARMAZENADAS AS OPERAÇÕES DENTRO DAS VARIÁVEIS
-
-soma = x + y
-sub = x - y
-divisao = x / y
-multi = x * y
-
-
-# AQUI SERÁ MOSTRADO PARA O USUÁRIO O RESULTADO DA OPERAÇÃO
-
 if operacao == 1:
-    print(f'O resultado da soma é {soma}')
+    print(f'O resultado da soma é {x + y}')
+
 elif operacao == 2:
-    print(f'O resultado da subtração é {sub}')
+    print(f'O resultado da subtração é {x - y}')
+
 elif operacao == 3:
-    print(f'O resultado da divisao é {divisao}')
+    if y == 0:
+        print('Não é possivel dividir por Zero!')
+    else:
+        print(f'O resultado da divisao é {x / y}')
+
 elif operacao == 4:
-    print(f'O resultado da multiplicação é {multi}')
+    print(f'O resultado da multiplicação é {x * y}')
+
 else:
     print('Selecione uma operação válida!')
